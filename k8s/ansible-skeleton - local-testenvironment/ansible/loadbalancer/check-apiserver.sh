37fae22430b2679eeb6deff3b9cfbe68495ad5ec
Vic Rottiers <vic.rottiers@student.hogent.be>
2022-03-01 12:20:40 +0100

    [update] k8s HA verder uitwerken

--- k8s/ansible-skeleton - local-testenvironment/ansible/loadbalancer/check-apiserver.sh
+++ k8s/ansible-skeleton - local-testenvironment/ansible/loadbalancer/check-apiserver.sh
@@ -5,7 +5,7 @@ errorExit() {
     exit 1
 }
 
-curl --silent --max-time 2 --insecure https://localhost:30843/ -o /dev/null || errorExit "Error GET https://localhost:30843/"
+curl --silent --max-time 2 --insecure https://localhost:6443/ -o /dev/null || errorExit "Error GET https://localhost:6443/"
 if ip addr | grep -q 192.168.56.111; then
-    curl --silent --max-time 2 --insecure https://192.168.56.111:30843/ -o /dev/null || errorExit "Error GET https://192.168.56.111:30843/"
+    curl --silent --max-time 2 --insecure https://192.168.56.111:6443/ -o /dev/null || errorExit "Error GET https://192.168.56.111:6443/"
 fi
